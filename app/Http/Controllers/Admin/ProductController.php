c19c41e8daea9168852e2d1fa6daff473134a731
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-12-24 07:46:41 -0800

    create an admin dashboard layout with tailwindcss and alpinejs

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -11,6 +11,6 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        return view('products.index' , compact('products'));
+        return view('admin.products.index' , compact('products'));
     }
 }
