4c5157c2964bcc3a7fa91fe183b3dbcedb3638c9
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-12-24 08:11:37 -0800

    create role model & migration & assign the role to a user

--- database/migrations/2022_12_24_154836_create_roles_table.php
+++ database/migrations/2022_12_24_154836_create_roles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,16 +14,17 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('role_id')->default(1);
-            $table->rememberToken();
             $table->timestamps();
         });
+        $roles = ['User' , 'Admin' , 'Writer'];
+        foreach ($roles as $role) {
+            Role::create([
+                'name' => $role
+            ]);
+        }
     }
 
     /**
@@ -32,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
